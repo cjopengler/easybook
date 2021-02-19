a402e85444266462cd176e23e53995100944af03
panxu <cjopengler@163.com>
2021-02-19 13:28:55 +0800

    inf

---
+++ daily/inf/inf_demo.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python 3
+# -*- coding: utf-8 -*-
+
+#
+# Copyright (c) 2021 PanXu, Inc. All Rights Reserved
+#
+"""
+brief
+
+Authors: PanXu
+Date:    2021/02/19 13:26:00
+"""
+
+import math
+
+inf = math.inf
+
+print(math.inf)
+print(-math.inf)
+
+assert inf > 1e20
+
+finf = float("inf")
+print(float("-inf"))
+
+assert inf == finf
+
+
+
+
